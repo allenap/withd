a6ea9d0071f9f19301c496538d5f4e45fa6a4660
Gavin Panella <gavin@allenap.me>
2024-06-10 10:59:43 +0200

    Generate completions with the `--completions <SHELL>` option

--- src/options.rs
+++ src/options.rs
@@ -2,6 +2,7 @@ use std::ffi::{OsStr, OsString};
 use std::path::PathBuf;
 
 use clap::{command, Parser, ValueHint};
+use clap_complete::Shell;
 
 #[derive(Parser)]
 #[command(
@@ -62,6 +63,14 @@ pub(crate) struct Options {
         default_value_t = false
     )]
     pub(crate) temporary: bool,
+
+    #[arg(
+        long,
+        hide = true,
+        help = "Generate shell completions.",
+        // exclusive = true
+    )]
+    pub(crate) completions: Option<Shell>,
 
     #[arg(
         help = "The command and its arguments.",
--- src/runner.rs
+++ src/runner.rs
@@ -1,8 +1,9 @@
 use std::env::{self, set_current_dir};
-use std::process::Command;
 use std::{fs::create_dir_all, path::Path};
+use std::{io::stdout, process::Command};
 
 use bstr::ByteSlice;
+use clap::CommandFactory;
 use lazy_regex::bytes_regex_captures;
 use tempfile::{Builder as TempBuilder, TempDir};
 
@@ -22,6 +23,13 @@ const TERMINATION_SIGNALS: &[Signal] = &[Signal::SIGINT, Signal::SIGQUIT, Signal
 
 /// Execute the command in the specified directory. Works on any platform.
 pub(crate) fn run(options: options::Options) -> Result<i32> {
+    if let Some(shell) = options.completions {
+        let mut command = options::Options::command();
+        let bin_name = command.get_name().to_string();
+        clap_complete::generate(shell, &mut command, bin_name, &mut stdout());
+        return Ok(0);
+    }
+
     // Ignore signals so that we're not terminated by them. We'll reenable them
     // later on in the child process, just before exec'ing.
     #[cfg(unix)]
