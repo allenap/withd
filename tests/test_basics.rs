8d8130a4481b77ac718f3ce8c3615269da02ce35
Gavin Panella <gavin@allenap.me>
2024-06-10 08:21:50 +0200

    Test `WHENCE` too

--- tests/test_basics.rs
+++ tests/test_basics.rs
@@ -59,6 +59,23 @@ fn test_run_temporary_with_create_in_non_existing_directory() {
     assert!(target.path().join("foo").join("here").exists());
 }
 
+#[test]
+fn test_whence() {
+    let target = TempDir::new().unwrap();
+    let path = target.path().join("foo").join("tmp.XXXXXX.dir");
+    let path_os = path.as_os_str();
+    let output = withd([os("-tc"), path_os, os("printenv"), os("WHENCE")]);
+    assert!(output.status.success());
+    // Convert stdout to a string from bytes. We assume that the path is valid
+    // UTF-8, but of course this is not guaranteed, so this test _may_ fail
+    // here. However, it's really unlikely.
+    let stdout = String::from_utf8(output.stdout).expect("Path is not valid UTF-8");
+    assert_eq!(
+        std::env::current_dir().expect("Could not get current directory"),
+        std::path::PathBuf::from(stdout.trim())
+    );
+}
+
 // -----------------------------------------------------------------------------
 
 static WITHD_EXE: &str = env!("CARGO_BIN_EXE_withd");
